f8fd7b9dfbfe5769cbb21fcad664240dc13c7ea2
NishanthDhanalakoti <sainishanth@puropalecreatons.com>
2025-03-13 09:53:40 +0530

    python files

---
+++ artha.py
@@ -0,0 +1,12 @@
+x= 50
+y= 10
+add= x+y
+sub= x-y
+mult=x*y
+div = x/y
+
+
+print(f"sum of the 2 numbers is {add}")
+print(f"difference of the 2 numbers is {sub}")
+print(f"the product of the 2 numbers is {mult}")
+print(f"the division of the 2 numbers is {div}")
